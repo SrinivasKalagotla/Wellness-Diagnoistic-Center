62844b0350c94911c3d6c99a8a5e59fe88f9efb2
SrinivasKalagotla <kalagotla.srinivas2821@gmail.com>
2023-04-29 22:02:26 -0500

    adfd

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 45354;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
